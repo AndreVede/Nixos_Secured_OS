e61d9892e4d3e5e3a1672dec9bce948bd6087b8d
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-03-10 13:26:35 +0100

    init: flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Standard Secured System";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default =
+    pkgs.mkShell {
+      buildInputs = with pkgs; [
+        neovim
+        vim
+      ];
+
+      ShellHook = ''
+        echo "Hello shell"
+      '';
+    };
+  };
+}
