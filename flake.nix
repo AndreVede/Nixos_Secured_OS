50997f9fafca88ed20b4fb7eefca371c4055434b
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-03-10 14:00:34 +0100

    feat: home-manager

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # nixpkgs.unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
@@ -23,6 +27,15 @@
 
       modules = [
         ./nixos/configuration.nix
+      ];
+    };
+
+    # use username
+    homeConfigurations."username" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [
+        ./home-manager/home.nix
       ];
     };
   };
