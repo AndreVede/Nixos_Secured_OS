0667b163377ad5204941b94293b4c9f89d1fc55e
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-03-10 13:53:47 +0100

    feat: transform to os

--- flake.nix
+++ flake.nix
@@ -2,24 +2,28 @@
   description = "Standard Secured System";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    # nixpkgs.unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = 
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          neovim
-          vim
-        ];
+    pkgs = import nixpkgs {
+      inherit system;
 
-        shellHook = ''
-          echo "Hello shell"
-        '';
+      config = {
+        allowUnfree = true;
       };
+    };
+  in {
+    nixosConfigurations = {
+      # replace with hostname
+      hostname = { inherit system; };
+
+      modules = [
+        ./nixos/configuration.nix
+      ];
+    };
   };
 }
