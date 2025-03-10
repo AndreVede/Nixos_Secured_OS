56f5dc2a80b07ea86fec04415a65317f3334a5db
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-03-10 23:00:41 +0100

    feat: system conf

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,6 +5,22 @@
       ./hardware-configuration.nix
     ];
 
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    users.users = {
+      username = {
+        isNormalUser = true;
+        extraGroups = ["wheel"];
+        initialPassword = "toto";
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      cowsay
+      lolcat
+    ];
+
     home-manager = {
       extraSpecialArgs = { inherit inputs; };
       users = {
